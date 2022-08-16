f3a31d1c412fd0a3beb01cda462db381bd809f1f
Gyuseob Song <gs2412@naver.com>
2022-08-16 18:07:39 +0900

    똥생성기 만들다 말았음

--- Assets/Scripts/Poops/PoopGen.cs
+++ Assets/Scripts/Poops/PoopGen.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PoopGen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float genTime;
+    public float genTimer;
+    public GameObject poopPrefab; //¶Ë ÇÁ¸®Æé ¿ÀºêÁ§Æ®
+    public float xRandomRange;
+
     void Start()
     {
         
@@ -13,6 +17,17 @@ public class PoopGen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        genTimer += Time.deltaTime;
+        if (genTimer >= genTime)
+        {
+            genTimer = 0;
+
+        }
+    }
+
+    private void genPoop(float y)
+    {
+        float x = Random.Range(-4f, 4f);
         
     }
 }
