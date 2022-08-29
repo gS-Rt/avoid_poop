b449a39db0f4f8f81372a0268bdb7015b241b555
Gyuseob Song <gs2412@naver.com>
2022-08-29 17:58:44 +0900

    3단 똥쌓고 삭제 완성
    뒤에 배치할 텍스트 스프라이트에 가려지는 방법 찾아야 할듯

--- Assets/Scripts/Poops/PoopsMove.cs
+++ Assets/Scripts/Poops/PoopsMove.cs
@@ -49,14 +49,16 @@ public class PoopsMove : MonoBehaviour
             isPutOn += 1;
             //this.gameObject.GetComponent<Rigidbody>().useGravity = false;
             //this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(50, 0, 0);
+
+            poopStack = 1; //플레이어 머리 위에 떨어지면 1층 똥
         }
-        else if (collision.gameObject.tag == "poop")
+        else if (collision.gameObject.tag == "poop" && poopStack == 0) //똥이 아직 충돌되지 않았을 때만 1회 실행
         {
-            if (collision.gameObject.GetComponent<PoopsMove>().poopStack == 0)
+            if (collision.gameObject.GetComponent<PoopsMove>().poopStack == 1)
             {
-                poopStack = 1;
+                poopStack = 2;
             }
-            else if (collision.gameObject.GetComponent<PoopsMove>().poopStack == 1)
+            else if (collision.gameObject.GetComponent<PoopsMove>().poopStack == 2)
             {
                 isBreak = true;
                 collision.gameObject.GetComponent<PoopsMove>().isBreak = true;
@@ -64,4 +66,19 @@ public class PoopsMove : MonoBehaviour
         }
 
     }
+
+    private void OnCollisionStay(Collision collision) //1층 똥을 위한 코드
+    {
+        if(collision.gameObject.GetComponent<PoopsMove>().isBreak==true) //2층 똥의 파괴변수가 참이 되면 같이 파괴
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    /*
+     * 현재는 3개 쌓으면 똥 파괴
+     * 그런데 똥탑을 계속 쌓다가 특정 상호작용을 하면 전부 터지면서 보너스 점수 들어가도록 해도 좋을듯
+     * 어차피 게이지의 감소 때문에 계속 모으기엔 리스크가 커질 수도 있을듯
+     */
+
 }
